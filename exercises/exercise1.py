b4f8be0598ae24d141cb473576559f5be566627a
Christoffer T. Jønsson <christofferctj@gmail.com>
2024-09-04 20:09:54 +0200

    Added my solution for exercise 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -23,9 +23,59 @@ class Gossip(Device):
         self._secrets = set([index])
 
     def run(self):
-        # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
+        # we will keep sending the secrets until we have all of them
+        while len(self._secrets) < self.number_of_devices():
+            # we send the secrets to all other devices
+            for i in range(0, self.number_of_devices()):
+                # we do not send to ourselves
+                if i == self.index():
+                    continue
+                # we send the secrets
+                message = GossipMessage(self.index(), i, self._secrets)
+                # we send the message via a "medium"
+                self.medium().send(message)
+                # while sending messages we also try to receive some messages
+                while True:
+                    ingoing = self.medium().receive()
+                    # if the medium returns None, we break the loop
+                    if ingoing is None:
+                        break
+                    self._secrets = self._secrets.union(ingoing.secrets)
+        return
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+class GossipLeftRight(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        # we will keep sending the secrets until we have all of them
+        while len(self._secrets) < self.number_of_devices():
+            # we can only send the secrets to its neighbors
+            left = (self.index() - 1) % self.number_of_devices()
+            right = (self.index() + 1) % self.number_of_devices()
+
+            # we send the secrets to the left
+            message = GossipMessage(self.index(), left, self._secrets)
+            self.medium().send(message)
+
+            # we send the secrets to the right
+            message = GossipMessage(self.index(), right, self._secrets)
+            self.medium().send(message)
+
+            # while sending messages we also try to receive some messages
+            while True:
+                ingoing = self.medium().receive()
+                # if the medium returns None, we break the loop
+                if ingoing is None:
+                    break
+                self._secrets = self._secrets.union(ingoing.secrets)
         return
 
     def print_result(self):
